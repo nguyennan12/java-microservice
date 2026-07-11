4bc6dbeb4654f36bcd8b7b0fefb7dd0c48271a27
nguyenan12 <nguyenann1204@gmail.com>
2026-07-11 17:54:53 +0700

    feat(api-gateway):implement api gatewat and security with keyloak

---
+++ api-gateway/src/main/java/com/techie/microservice/gateway/routes/Routes.java
@@ -0,0 +1,39 @@
+package com.techie.microservice.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoutes() {
+        return GatewayRouterFunctions.route("product-service")
+                .route(path("/api/product/**"), http())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoutes() {
+        return GatewayRouterFunctions.route("order-service")
+                .route(path("/api/order/**"), http())
+                .before(uri("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoutes() {
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(path("/api/inventory/**"), http())
+                .before(uri("http://localhost:8082"))
+                .build();
+    }
+}
