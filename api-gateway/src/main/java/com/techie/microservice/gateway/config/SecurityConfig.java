d0e498a28737a21919c04d3ebdd28809e84d1ed6
nguyenan12 <nguyenann1204@gmail.com>
2026-07-11 20:53:26 +0700

    fix: configure CORS to allow requests from Angular client

--- api-gateway/src/main/java/com/techie/microservice/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/techie/microservice/gateway/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
